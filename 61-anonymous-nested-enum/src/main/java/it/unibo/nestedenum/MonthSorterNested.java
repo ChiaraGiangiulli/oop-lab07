f7d3641fd758407e44d17b2f4d0af0f666199e3c
ChiaraGiangiulli <chiara.giangiulli@studio.unibo.it>
2022-11-08 18:40:52 +0100

    modified 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java

--- 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -1,7 +1,6 @@
 package it.unibo.nestedenum;
 
 import java.util.Comparator;
-import java.util.Locale;
 import java.util.Objects;
 
 /**
@@ -23,25 +22,64 @@ public final class MonthSorterNested implements MonthSorter {
         NOVEMBER(30),
         DECEMBER(31);
 
-        private static int days; 
+        private int days; 
         
         Month(int n){
             this.days=n;
         }
 
-        Month fromString(String s){
-            
-            return 
+        static Month fromString(String s){
+            Objects.requireNonNull(s);
+            s.toUpperCase();
+            char character;
+            Month match = null;
+            try {
+                return valueOf(s);
+            } catch (IllegalArgumentException exc){
+                for (final Month month: values()) {
+                    character = month.toString().charAt(0);
+                    if(s.startsWith(String.valueOf(character))){
+                        if(match == null){
+                            match = month;
+                        }
+                        else{
+                            throw new IllegalArgumentException(
+                                s + " is ambiguous: both " + match + " and " + month + " would be valid matches", exc);
+                        }
+                    }
+                }
+                if (match == null) {
+                    throw new IllegalArgumentException("No matching months for " + s, exc);
+                }
+                return match;
+            }
+        }
+    }
+
+    private static class SortByDays implements Comparator<String> {
+        @Override
+        public int compare(String arg0, String arg1) {
+            Month m0 = Month.fromString(arg0);
+            Month m1 = Month.fromString(arg1);
+            return Integer.compare(m0.days, m1.days);
+        }
+    }
+
+    private static class SortByMonthOrder implements Comparator<String> {
+
+        @Override
+        public int compare(String arg0, String arg1) {
+            return Month.fromString(arg0).compareTo(Month.fromString(arg1));
         }
     }
 
     @Override
     public Comparator<String> sortByDays() {
-        return null;
+        return new SortByDays();
     }
 
     @Override
     public Comparator<String> sortByOrder() {
-        return null;
+        return new SortByMonthOrder();
     }
 }
